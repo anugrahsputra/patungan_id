a6b4ee46099c75d3cbbbf6fafb14ef4a3812f16b
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2023-10-19 15:40:23 +0700

    fix: change file name

--- lib/app/domain/usecase/auth/auth.dart
+++ lib/app/domain/usecase/auth/auth.dart
@@ -1,2 +1,2 @@
-export 'signIn_usecase.dart';
+export 'sign_in_usecase.dart';
 export 'verify_otp_usecase.dart';
--- test/domain/usecase/verify_otp_usecase_test.dart
+++ test/domain/usecase/verify_otp_usecase_test.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:patungan_id/app/core/core.dart';
 import 'package:patungan_id/app/domain/domain.dart';
 
 import '../../helper/mock.mocks.dart';
@@ -14,6 +16,8 @@ void main() {
     usecase = VerifyOtpUsecase(mockAuthRepository);
   });
 
+  const otp = '123456';
+
   test('should verify otp from the repository', () async {
     const otp = '123456';
 
@@ -23,5 +27,19 @@ void main() {
     final result = await usecase.call(otp);
 
     expect(result, const Right(null));
+    verify(mockAuthRepository.verifyOtp(otp));
+    verifyNoMoreInteractions(mockAuthRepository);
+  });
+
+  test('should return failure when verify otp fails', () async {
+    final exception = FirebaseAuthException(code: 'code', message: 'message');
+    when(mockAuthRepository.verifyOtp(any))
+        .thenAnswer((_) async => Left(ServerFailure(exception.message!)));
+
+    final result = await usecase.call(otp);
+
+    expect(result, Left(ServerFailure(exception.message!)));
+    verify(mockAuthRepository.verifyOtp(otp));
+    verifyNoMoreInteractions(mockAuthRepository);
   });
 }
