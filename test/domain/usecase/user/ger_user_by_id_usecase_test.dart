b81899a616272f8077c8acc706247ca462dbc338
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2023-11-04 18:41:43 +0700

    Remove unused imports in user_cubit.dart and ger_user_by_id_usecase_test.dart

--- test/domain/usecase/user/ger_user_by_id_usecase_test.dart
+++ test/domain/usecase/user/ger_user_by_id_usecase_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:patungan_id/app/domain/domain.dart';
-import 'package:patungan_id/app/domain/usecase/user/user.dart';
 
 import '../../../helper/mock.mocks.dart';
 
